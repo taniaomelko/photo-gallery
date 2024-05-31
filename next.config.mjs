754cd48546237ad6923cdb77e76b92833f2a2618
Tania Omelko <tanyaomelko39@gmail.com>
2024-05-31 17:20:49 +0300

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/photo-gallery",
+  output: "export",
+  reactStrictMode: true,
   images: {
     domains: ['images.pexels.com'],
   },
